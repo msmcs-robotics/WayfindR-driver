08f1510e7d8587403133a964d936c5bf90c40288
gndpwnd <88689368+gndpwnd@users.noreply.github.com>
2025-05-24 14:27:18 -0800

    use MPU6050 with RPI directly

---
+++ sensor_telem/mpu6050_rpi.py
@@ -0,0 +1,44 @@
+
+# MPU - RPI
+# VCC - GPIO 1
+# GND - GPIO 6
+# SDA - GPIO 2
+# SCL - GPIO 3
+
+# sudo apt install python3-smbus
+# pip install mpu6050-raspberrypi
+
+import mpu6050
+import time
+
+I2C_address = 0x68
+
+# Create a new Mpu6050 object
+mpu6050 = mpu6050.mpu6050(I2C_address)
+
+# Define a function to read the sensor data
+def read_sensor_data():
+    # Read the accelerometer values
+    accelerometer_data = mpu6050.get_accel_data()
+
+    # Read the gyroscope values
+    gyroscope_data = mpu6050.get_gyro_data()
+
+    # Read temp
+    temperature = mpu6050.get_temp()
+
+    return accelerometer_data, gyroscope_data, temperature
+
+# Start a while loop to continuously read the sensor data
+while True:
+
+    # Read the sensor data
+    accelerometer_data, gyroscope_data, temperature = read_sensor_data()
+
+    # Print the sensor data
+    print("Accelerometer data:", accelerometer_data)
+    print("Gyroscope data:", gyroscope_data)
+    print("Temp:", temperature)
+
+    # Wait for 1 second
+    time.sleep(1)
